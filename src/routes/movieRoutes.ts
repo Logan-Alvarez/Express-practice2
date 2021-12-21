38932d3294bc4a303045f52a2f8e41033d0aa87c
Logan Alvarez <logan.alvarez616@gmail.com>
2021-12-21 14:59:52 -0500

    Created interface and array for Movies

---
+++ src/routes/movieRoutes.ts
@@ -0,0 +1,10 @@
+import { Movie } from "../models/movieInterface";
+
+const MoviesList: Movie[] = [
+    {id:1, title:"Lion King", year:1994, animated: true },
+    {id:2, title:"Toy story", year:1995, animated: true },
+    {id:3, title:"Avengers", year:2012, animated: false },
+    {id:4, title:"Jurassic Park", year:1993, animated: false },
+    {id:5, title:"Black Panther", year:2018, animated: false }
+];
+
